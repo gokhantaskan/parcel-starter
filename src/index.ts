d7d139e8be9eb7a4e578bd53fe914bfd434d6d17
Gökhan Taşkan <gokhan.frontfeed@gmail.com>
2019-12-16 23:32:55 +0300

    async await iife test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { injectSvg } from "./utils";
 
 // Trigger dark theme with button click
 const btn: HTMLElement | null = document.getElementById("btn");
-const htmlBody: HTMLElement | null = document.querySelector("body");
+const htmlBody = document.querySelector("body");
+const headline = document.querySelector("h1");
 
 if (btn && htmlBody) {
   btn.addEventListener("click", () => {
@@ -11,5 +12,13 @@ if (btn && htmlBody) {
   });
 }
 
-// ex. <img class="inj" src="x.svg" /> will be rendered
+(async () => {
+  await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then(response => response.json())
+    .then(json => {
+      if (headline) headline.innerText = json.title;
+    });
+})();
+
+// ie <img class="inj" src="x.svg" /> will be rendered as an inline SVG
 injectSvg("inj");
